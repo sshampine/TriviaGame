d872d2e55da72a84fd6fb6a3c9f7b2e6b2c90f84
Scott Shampine <sshampine@gmail.com>
2017-08-27 22:39:05 -0500

    added files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,52 +2,62 @@
 
 var q1 = {
 	question: "The theme for Star Trek The Next Generation was taken from which Star Trek feature movie?",
-	answerOptions: ['The Motion Picture', 'The Wrath of Kahn', 'The Search for Spock', 'The Voyage Home']
+	answerOptions: ['The Motion Picture', 'The Wrath of Kahn', 'The Search for Spock', 'The Voyage Home'],
+	answer: [true, false, false, false]
 };
 
 var q2 = {
 	question: 'What is the name of the Vulcan ritual that purges all emotions?',
-	answerOptions: ['Kolinar', 'Ponfar','Kalifee', 'Katra']
+	answerOptions: ['Kolinar', 'Ponfar','Kalifee', 'Katra'],
+	answer: [true, false, false, false]
 };
 
 var q3 = {
 	question: 'Star Trek II The Wrak of Kahn is loosly a sequal to which original episode?',
-	answerOptions: ['Amok Time', 'Space Seed', 'The Trouble with Tribbles', 'Miri']
+	answerOptions: ['Amok Time', 'Space Seed', 'The Trouble with Tribbles', 'Miri'],
+	answer: [false, true, false, false]
 };
 
 var q4 = {
 	question: 'In Star Trek The Next Generation, who is the creator of Data?',
-	answerOptions: ['Kahn Noonian Singh', 'Noonian Soong', 'Beverly Crusher', 'Christopher Metzen']
+	answerOptions: ['Kahn Noonian Singh', 'Noonian Soong', 'Beverly Crusher', 'Christopher Metzen'],
+	answer: [false, true, false, false]
 };
 
 var q5 = {
 	question: 'In the original series, what class ship was the Enterprise?',
-	answerOptions: ['Enterprise class', 'Galaxy class', 'Consitution class', 'Defiant class']
+	answerOptions: ['Enterprise class', 'Galaxy class', 'Consitution class', 'Defiant class'],
+	answer: [false, false, true, false]
 };
 
 var q6 = {
 	question: 'What is the name of the first Klingon in Starfleet?',
-	answerOptions: ['Woof', 'Targ', 'Kempek', 'Worf']
+	answerOptions: ['Woof', 'Targ', 'Kempek', 'Worf'],
+	answer: [false, false, false, true]
 };
 
 var q7 = {
 	question: 'What was the name of the episode where Kirk fought a Gorn?',
-	answerOptions: ['Arena', 'Let That Be Your Last Battlefield', 'Mirror, Mirror', 'The Balance of Terror']
+	answerOptions: ['Arena', 'Let That Be Your Last Battlefield', 'Mirror, Mirror', 'The Balance of Terror'],
+	answer: [true, false, false, false]
 };
 
 var q8 = {
 	question: 'In what quadrant of space was the USS Voyage trapped?',
-	answerOptions: ['Alpha', 'Beta', 'Delta', 'Gamma']
+	answerOptions: ['Alpha', 'Beta', 'Delta', 'Gamma'],
+	answer: [false, false, true, false]
 };
 
 var q9 = {
 	question: 'What race build then later abandoned the DS9 spacestation?',
-	answerOptions: ['Vulcans', 'Andorians', 'Cardassians', 'The Founders']
+	answerOptions: ['Vulcans', 'Andorians', 'Cardassians', 'The Founders'],
+	answer: [false, false, true, false]
 };
 
 var q10 = {
 	question: 'In Star Trek IV The Voyage Home, what rank was Admirial Kirk demoted to for disobeying orders?',
-	answerOptions: ['Ensine', 'Lutenent', 'Commander', 'Captain']
+	answerOptions: ['Ensine', 'Lutenent', 'Commander', 'Captain'],
+	answer: [false, false, false, true]
 };
 
 // other variables
@@ -72,13 +82,28 @@ var questionArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 
 function question() {
 $('#question-area').html(questionArray[index].question);
-$('#answerOne').html(questionArray[index].answerOptions[0])
-$('#answerTwo').html(questionArray[index].answerOptions[1])
-$('#answerThree').html(questionArray[index].answerOptions[2])
-$('#answerFour').html(questionArray[index].answerOptions[3])
+for (var i = 0; i < 4; i++) {
+ 	var answerDiv = $('<div>');
+
+ 	answerDiv.addClass('test');
+ 	answerDiv.html(questionArray[index].answerOptions[i])
+ 	answerDiv.attr('data-answer', questionArray[index].answer[i])
+ 	$('#answer-area').append(answerDiv);
+}
+
+
+
+//$('#answerOne').html(questionArray[index].answerOptions[0])
+//$('#answerTwo').html(questionArray[index].answerOptions[1])
+//$('#answerThree').html(questionArray[index].answerOptions[2])
+//$('#answerFour').html(questionArray[index].answerOptions[3])
+
+
+
+
 
 $('#answerOne').on('click', function() {
-	
+	var ansOne = 
 	console.log("you clicked " + questionArray[0].answerOptions[0])
 });
 
